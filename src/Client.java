7d9ee68eaf3c56b6a2e9a9be3cf8a35387da22bb
Lor3nzoRossi <lorenzoistruzione@gmail.com>
2024-03-01 14:15:37 +0100

    primo commit

---
+++ src/Client.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Studenti
+ */
+public class Client {
+    String nome;
+    String colore;
+
+    public Client(String nomeDefault, String coloreDefault) {
+        this.nome = nomeDefault;
+        this.colore = coloreDefault;
+    }
+    public void connetti(String nomeServer, int portaServer){
+        
+    }
+    public void scrivi(){
+        
+    }
+    public void leggi(){
+        
+    }
+    public void chiudi(){
+        
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,37 @@
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Studenti
+ */
+public class Server {
+    ServerSocket serverSocket;
+    Socket socket;
+    int porta;
+    
+    public Server(int porta){
+        this.porta = porta;
+    }
+    public Socket attendi(){
+        return null;
+    }
+    public void scrivi(){
+        
+    }
+    public void leggi(){
+        
+    }
+    public void chiudi(){
+        
+    }
+    public void termina(){
+        
+    }
+}
